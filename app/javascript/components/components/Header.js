d0f537bab66a9a4788e5108cc590014fe2a93ba2
josesison1 <learnacademy@LEARNs-Air.lan>
2022-04-12 22:06:39 -0700

    updates

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Nav, NavItem } from 'reactstrap'
 
 class Header extends Component {
     render() {
@@ -14,21 +13,11 @@ class Header extends Component {
         console.log("current_user:", current_user)
         return (
             <>
-                <h1>Welcome to my Apartment Application!</h1>
-                <Nav>
-                    <NavItem>
-                        <a href={sign_in_route} id="links" className="nav-link">Sign In</a>
-                    </NavItem>
-
-                    <NavItem>
-                        <a href={new_user_route} className="nav-link">Sign Up</a>
-                    </NavItem>
-
-                    <NavItem>
-                        <a href={sign_out_route} className="nav-link">Sign Out</a>
-                    </NavItem>
-
-                </Nav>
+                <div className="header">
+                    <h1>Welcome to my Apartment Application!</h1>
+                    <div className="headertext">
+                        <p>Find your peace in San Diego. Inspired by the rustic charm of Tuscany and the coastal vibe of the fishing village that shares its name, this apartment community takes you on a journey across the ocean to a picturesque Italian paradise. Vivi la dolce vita surrounded by towering palms, cobblestone accents, and trickling fountains at Magic City.</p>
+                    </div></div>
             </>
         )
     }
