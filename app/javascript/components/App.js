60cac73c7bb564fb786545cf0a3da46ce08bdf46
josesison1 <learnacademy@LEARNs-Air.lan>
2022-04-13 11:20:15 -0700

    completed navigation

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import Home from './pages/Home'
 import Header from './components/Header'
+import Navigation from './components/Navigation'
+import Apartments from './pages/Apartments'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <>
         <Header {...this.props} />
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Apartments" component={Apartments} />
+          </Switch>
+        </Router>
       </>
     )
   }
